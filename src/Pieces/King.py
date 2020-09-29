0668c2acd0fc831f6f2e137116f234d74da07ccf
deanquaife <deanquaife@outlook.com>
2020-09-29 10:54:19 +0800

    Added all Pieces; is_valid_move no longer takes other pieces into account

--- src/Pieces/King.py
+++ src/Pieces/King.py
@@ -2,24 +2,27 @@
 Last Edited: 24/09/2020"""
 from src.Pieces.Piece import Piece
 
-class Rook(Piece):
-
-    """Rook constructor: Creates a new rook
-    Rooks have a field m_moved which indicates whether they have moved yet this game.
+class King(Piece):
+    """King constructor: Creates a new king
     x, y, player: see Piece.py"""
+
     def __init__(self, x, y, player):
         super(x, y, player)
-        self.m_type = "ROOK"
-        self.m_moved = False
+        self.m_type = "KING"
 
     def move(self, newX, newY, board):
         pass
 
-    """Check whether a rook can make this move
+    """Check whether a king can make this move
     newX: the X coordinate we're trying to move to
     newY: the Y coordinate we're trying to move to
-    return: a boolean indicating whether the rook can make this move"""
+    return: a boolean indicating whether the king can make this move"""
+
     def is_valid_move(self, newX, newY):
-        if self.m_x == newX or self.m_y == newY:
+        x_diff = abs(newX, self.m_x)
+        y_diff = abs(newY, self.m_y)
+
+        if x_diff < 2 and y_diff < 2:
             return True
+
         return False
--- src/Pieces/Knight.py
+++ src/Pieces/Knight.py
@@ -2,24 +2,25 @@
 Last Edited: 24/09/2020"""
 from src.Pieces.Piece import Piece
 
-class Rook(Piece):
+class Knight(Piece):
 
-    """Rook constructor: Creates a new rook
-    Rooks have a field m_moved which indicates whether they have moved yet this game.
+    """Knight constructor: Creates a new knight
     x, y, player: see Piece.py"""
     def __init__(self, x, y, player):
         super(x, y, player)
-        self.m_type = "ROOK"
-        self.m_moved = False
+        self.m_type = "KNIGHT"
 
     def move(self, newX, newY, board):
         pass
 
-    """Check whether a rook can make this move
+    """Check whether a knight can make this move
     newX: the X coordinate we're trying to move to
     newY: the Y coordinate we're trying to move to
-    return: a boolean indicating whether the rook can make this move"""
+    return: a boolean indicating whether the knight can make this move"""
     def is_valid_move(self, newX, newY):
-        if self.m_x == newX or self.m_y == newY:
+        x_diff = abs(newX, self.m_x)
+        y_diff = abs(newY, self.m_y)
+
+        if (x_diff == 1 and y_diff == 2) or (x_diff == 2 and y_diff == 1):
             return True
         return False
